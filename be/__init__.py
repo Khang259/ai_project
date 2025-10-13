00479f3ef8be651aca05e3bec227c8b678588dd3
Tạ Minh Công <cong23637@gmail.com>
2025-10-13 14:19:16 +0700

    chore: stop tracking backend folder

--- be/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Backend package for CameraAI
-Microservices architecture with shared modules
-"""
-
-__version__ = "1.0.0"
-__author__ = "Your Name"
-__description__ = "CameraAI Backend with FastAPI microservices"
