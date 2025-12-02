2cbc511e7120aafaec533e00d0da17c6dbf0329c
LamPhamNgoc1310 <phamngoclam1310@gmail.com>
2025-12-02 16:59:04 +0700

    docs: add Router.jsx for better routes management

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,33 +1,22 @@
 import { useEffect, useState } from "react";
-import { Routes, Route } from 'react-router-dom';
-import DashboardLayout from './components/DashboardLayout';
-import Dashboard from './pages/DashBoard';
-import Analytics from './pages/Analytics';
-import TaskManagement from './pages/TaskManagement';
-import Notification from './pages/Notification';
-import Users from './pages/Users';
-import Settings from './pages/Settings';
-import LoginPage from './pages/Login';
-import Maintain from './pages/Maintain';
-import MonitorPackaged from './pages/MonitorPackaged';
-import MobileGridDisplay from './pages/MobileGridDisplay';
-import MonitorPage from './pages/Monitor';
-import Area from './pages/Area';
-import PrivateRoute from './components/PrivateRoute';
-import { AreaProvider } from './contexts/AreaContext';
-import { LanguageProvider } from './contexts/LanguageContext';
-import './i18n';
+import { Routes, Route } from "react-router-dom";
+
+import routes from "./router.jsx";
+import { AreaProvider } from "./contexts/AreaContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
+import "./i18n";
 
 function App() {
   const [showVideo, setShowVideo] = useState(false);
+
   useEffect(() => {
-    // Khi React mount xong, bật cờ hiển thị video
     setShowVideo(true);
   }, []);
+
   return (
     <LanguageProvider>
       <AreaProvider>
-        {/* Background video */}
+          {/* Background video */}
         {/* {showVideo && (
           <video
             id="background-video"
@@ -36,126 +25,16 @@ function App() {
             loop
             playsInline
             className="fixed top-0 left-0 w-full h-full object-cover -z-10 opacity-0 transition-opacity duration-700"
-            onCanPlay={(e) => (e.target.style.opacity = 1)} // fade-in khi sẵn sàng
+            onCanPlay={(e) => (e.target.style.opacity = 1)}
           >
             <source src="/src/assets/vid_bg.mp4" type="video/mp4" />
           </video>
         )} */}
         <div className="fixed top-0 left-0 w-full h-full bg-black -z-10"></div>
         <Routes>
-          {/* Public routes */}
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/" element={<LoginPage />} />
-
-      {/* User routes - Mobile Grid Display */}
-      <Route
-        path="/mobile-grid-display"
-        element={
-          <PrivateRoute>
-            <MobileGridDisplay />
-          </PrivateRoute>
-        }
-      />
-
-      {/* User routes - Production Monitor (read-only) */}
-      <Route
-        path="/monitor"
-        element={
-          // <PrivateRoute requiredRole="viewer">
-            <MonitorPage />
-          // </PrivateRoute>
-        }
-      />
-
-          {/* Admin routes - Dashboard Layout */}
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute requiredRole={["admin", "operator"]}>
-                <DashboardLayout>
-                  <Dashboard />
-                </DashboardLayout>
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/maintain"
-            element={
-              <PrivateRoute requiredRole={["admin", "operator"]}>
-                <DashboardLayout>
-                  <Maintain />
-                </DashboardLayout>
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/analytics"
-            element={
-              <PrivateRoute requiredRole={["admin", "operator"]}>
-                <DashboardLayout>
-                  <Analytics />
-                </DashboardLayout>
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/task-management"
-            element={
-              <PrivateRoute requiredRole={["admin", "operator"]}>
-                <DashboardLayout>
-                  <TaskManagement />
-                </DashboardLayout>
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/notification"
-            element={
-              <PrivateRoute requiredRole={["admin", "operator"]}>
-                <DashboardLayout>
-                  <Notification />
-                </DashboardLayout>
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/users"
-            element={
-              <PrivateRoute requiredRole={["admin", "operator"]}>
-                <DashboardLayout>
-                  <Users />
-                </DashboardLayout>
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/area"
-            element={
-              <PrivateRoute requiredRole={["admin"]}>
-                <DashboardLayout>
-                  <Area />
-                </DashboardLayout>
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/settings"
-            element={
-              <PrivateRoute requiredRole={["admin", "operator"]}>
-                <DashboardLayout>
-                  <Settings />
-                </DashboardLayout>
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/monitor-packaged"
-            element={
-              <PrivateRoute>
-                <MonitorPackaged />
-              </PrivateRoute>
-            }
-          />
+          {routes.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
         </Routes>
       </AreaProvider>
     </LanguageProvider>
